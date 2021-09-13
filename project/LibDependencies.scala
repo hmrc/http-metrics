ec6d18d22717922b04419fb404e3e9b83f5dd9cd
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2021-09-13 08:33:11 +0100

    APIS-5440 - Uplift to play2_8 in cross compile

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -3,6 +3,7 @@ import sbt._
 object LibDependencies {
   val play26Version = "2.6.25"
   val play27Version = "2.7.9"
+  val play28Version = "2.8.7"
 
   val coreCompileCommon: Seq[ModuleID] = Seq(
     "com.kenshoo"             %% "metrics-play"           % "2.6.6_0.6.2",
@@ -19,10 +20,15 @@ object LibDependencies {
     "com.typesafe.play"     %% "play-guice"            % play27Version
   )
   
+  val coreCompilePlay28: Seq[ModuleID] = Seq(
+    "com.typesafe.play"     %% "play"                  % play28Version,
+    "com.typesafe.play"     %% "play-guice"            % play28Version
+  )
+
   val coreTestCommon: Seq[ModuleID] = Seq(
-    "org.pegdown"             % "pegdown"                 % "1.6.0" % Test,
-    "com.github.tomakehurst"  % "wiremock"                % "2.8.0" % Test,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.7.1" % Test
+    "org.pegdown"             % "pegdown"                   % "1.6.0" % Test,
+    "com.github.tomakehurst"  % "wiremock-jre8-standalone"  % "2.27.1" % Test,
+    "org.mockito"            %% "mockito-scala-scalatest"   % "1.7.1" % Test
   )
 
   val coreTestPlay26: Seq[ModuleID] = Seq(
@@ -35,4 +41,9 @@ object LibDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play"      % "4.0.3" % Test
   )
 
+  val coreTestPlay28: Seq[ModuleID] = Seq(
+    "com.typesafe.play"      %% "play-test"               % play28Version % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0" % Test,
+    "com.vladsch.flexmark"   %  "flexmark-all"            % "0.36.8" % Test
+  )
 }
