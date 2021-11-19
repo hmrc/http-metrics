29413765f5b5f2dfd1132afbbff86ee42940e2d9
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2021-11-19 13:51:05 +0000

    APIS-5447 - Upgrade play 2.7/2.8 metrics libraries

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -6,21 +6,25 @@ object LibDependencies {
   val play28Version = "2.8.7"
 
   val coreCompileCommon: Seq[ModuleID] = Seq(
-    "com.kenshoo"             %% "metrics-play"           % "2.6.6_0.6.2",
-    "io.dropwizard.metrics"   %  "metrics-graphite"       % "3.2.5"
-  )
-
+    )
+    
   val coreCompilePlay26: Seq[ModuleID] = Seq(
+    "com.kenshoo"           %% "metrics-play"          % "2.6.6_0.6.2",
+    "io.dropwizard.metrics" %  "metrics-graphite"      % "3.2.5",
     "com.typesafe.play"     %% "play"                  % play26Version,
     "com.typesafe.play"     %% "play-guice"            % play26Version
   )
 
   val coreCompilePlay27: Seq[ModuleID] = Seq(
+    "com.kenshoo"           %% "metrics-play"          % "2.7.3_0.8.2",
+    "io.dropwizard.metrics" %  "metrics-graphite"      % "4.0.5",
     "com.typesafe.play"     %% "play"                  % play27Version,
     "com.typesafe.play"     %% "play-guice"            % play27Version
-  )
-  
-  val coreCompilePlay28: Seq[ModuleID] = Seq(
+    )
+    
+    val coreCompilePlay28: Seq[ModuleID] = Seq(
+    "com.kenshoo"           %% "metrics-play"          % "2.7.3_0.8.2",
+    "io.dropwizard.metrics" %  "metrics-graphite"      % "4.1.17",
     "com.typesafe.play"     %% "play"                  % play28Version,
     "com.typesafe.play"     %% "play-guice"            % play28Version
   )
@@ -35,7 +39,7 @@ object LibDependencies {
     "com.typesafe.play"      %% "play-test"               % play26Version % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "3.1.3" % Test
   )
-    
+
   val coreTestPlay27: Seq[ModuleID] = Seq(
     "com.typesafe.play"      %% "play-test"               % play27Version % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "4.0.3" % Test
