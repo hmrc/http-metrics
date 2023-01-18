b79d1b47e8bdc0918d89e8ac43997d61ed2b638a
John Green <john.green@digital.hmrc.gov.uk>
2023-01-18 18:31:11 +0000

    API-6413: Add scalastyle config

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-SBT_OPTS="-XX:MaxMetaspaceSize=400M" sbt clean scalafmtAll scalafixAll coverage test coverageReport
+SBT_OPTS="-XX:MaxMetaspaceSize=400M" sbt clean scalastyle scalafmtAll scalafixAll coverage test coverageReport
